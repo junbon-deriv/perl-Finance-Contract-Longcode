57cba744eaab1bb5e9806596da3decd81f737e58
Jun Bon <junbon@binary.com>
2019-05-31 01:51:39 +0000

    save stop_out_barrier in shortcode

--- lib/Finance/Contract/Longcode.pm
+++ lib/Finance/Contract/Longcode.pm
@@ -169,10 +169,9 @@ sub shortcode_to_parameters {
     $is_sold //= 0;
 
     my (
-        $bet_type,     $underlying_symbol,    $payout,        $date_start,          $date_expiry,
-        $barrier,      $barrier2,             $fixed_expiry,  $duration,            $contract_multiplier,
-        $product_type, $trading_window_start, $selected_tick, $stop_out_percentage, $entry_spot,
-        $stake,        $multiplier,           $auto_renew,    $insurance_total_cost
+        $bet_type,      $underlying_symbol, $payout,   $date_start,          $date_expiry,  $barrier,
+        $barrier2,      $fixed_expiry,      $duration, $contract_multiplier, $product_type, $trading_window_start,
+        $selected_tick, $entry_spot,        $stake,    $multiplier,          $auto_renew,   $insurance_total_cost
     );
 
     my $forward_start = 0;
@@ -198,14 +197,14 @@ sub shortcode_to_parameters {
         $barrier              = $8;
         $insurance_total_cost = $9;
         $auto_renew           = $10;
-    } elsif ($shortcode =~ /^(MULTUP|MULTDOWN)_(R?_?[^_\W]+)_(\d*\.?\d*)_(\d+)_(\d+)_(\d*\.?\d*)_(\d+)$/) {
-        $bet_type            = $1;
-        $underlying_symbol   = $2;
-        $stake               = $3;
-        $multiplier          = $4;
-        $date_start          = $5;
-        $stop_out_percentage = $6;
-        $entry_spot          = $7;
+    } elsif ($shortcode =~ /^(MULTUP|MULTDOWN)_(R?_?[^_\W]+)_(\d*\.?\d*)_(\d+)_(\d+)_(\d+)_(\d+)$/) {
+        $bet_type          = $1;
+        $underlying_symbol = $2;
+        $stake             = $3;
+        $multiplier        = $4;
+        $date_start        = $5;
+        $entry_spot        = $6;
+        $barrier           = $7;
     } elsif ($shortcode =~
         /^([^_]+)_([\w\d]+)_(\d*\.?\d*)_(\d+)(?<start_cond>[F]?)_(\d+)(?<expiry_cond>[FT]?)_(S?-?\d+P?)_(S?-?\d+P?)(?:_(?<extra>[PM])(\d*\.?\d+))?$/)
     {    # Both purchase and expiry date are timestamp (e.g. a 30-min bet)
@@ -305,11 +304,10 @@ sub shortcode_to_parameters {
     }
 
     if ($bet_type =~ /^MULT(?:UP|DOWN)$/) {
-        $bet_parameters->{amount_type}         = 'stake';
-        $bet_parameters->{amount}              = $stake;
-        $bet_parameters->{multiplier}          = $multiplier;
-        $bet_parameters->{stop_out_percentage} = $stop_out_percentage;
-        $bet_parameters->{entry_spot}          = _strike_string($entry_spot, $underlying_symbol, $bet_type);
+        $bet_parameters->{amount_type} = 'stake';
+        $bet_parameters->{amount}      = $stake;
+        $bet_parameters->{multiplier}  = $multiplier;
+        $bet_parameters->{entry_spot}  = _strike_string($entry_spot, $underlying_symbol, $bet_type);
     }
 
     if ($bet_type =~ /^INS(?:UP|DOWN)$/) {
